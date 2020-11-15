78a63800d17ee6360490f3c91d5782ee41d91b0c
kareemegy <kokekarim@yahoo.com>
2020-11-15 11:35:09 +0200

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,45 @@
 <template>
-  <div class="header">
-    <!-- there will be a header at v2 of the app we're currently at v1 -->
+  <div class="header ">
+    <div class="container">
+      <div class="col-1">
+        <img
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
+          alt="logo"
+        />
+        <ul class="nav_info col-2">
+          <li>Movies</li>
+          <li>TV Shows</li>
+          <li>People</li>
+          <li>More</li>
+        </ul>
+      </div>
+      <ul class="nav_icons col-2">
+        <li>
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/1828/1828817.svg"
+            alt="plus Icon"
+          />
+        </li>
+        <li>
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/889/889588.svg"
+            alt="lang icon "
+          />
+        </li>
+        <li>
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/599/599305.svg"
+            alt="profile icon"
+          />
+        </li>
+        <li>
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/639/639375.svg"
+            alt="search icon"
+          />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,11 +47,52 @@
 export default {};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.container {
+  width: 1300px;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
 .header {
   width: 100%;
   height: 70px;
   background-color: #032541;
   margin-bottom: 10px;
+}
+li {
+  list-style: none;
+}
+
+img {
+  width: 150px;
+  height: 20px;
+}
+.col-1 {
+  img {
+    margin-left: 20px;
+  }
+  display: flex;
+  align-items: center;
+  color: white;
+  ul {
+    margin-left: 10px;
+    li {
+      padding: 10px;
+    }
+  }
+}
+
+.col-2 {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding-right: 20px;
+
+  img {
+    margin-right: 25px;
+    width: 40px;
+    height: 25px;
+  }
 }
 </style>
